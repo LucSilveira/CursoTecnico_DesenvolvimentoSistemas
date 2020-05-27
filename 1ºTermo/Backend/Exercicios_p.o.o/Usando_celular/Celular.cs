ae6cd81e0ab722c37dc79bfd84df1e18f0e12db5
LucSilveira <lucas1-portal@hotmail.com>
2020-05-27 11:24:20 -0300

    Subindo correção dos comentários

--- 1ºTermo/Backend/Exercicios_p.o.o/Usando_celular/Celular.cs
+++ 1ºTermo/Backend/Exercicios_p.o.o/Usando_celular/Celular.cs
@@ -14,10 +14,15 @@ namespace Usando_celular
         public bool estadoAparelho;
 
 
+        /// <summary>
+        /// Método para interpretar se o usuário quer ligar o aparelho
+        /// </summary>
+        /// <param name="aparelho"></param>
+        /// <returns>Celular ligado(sim/true) ou continuar desligado(não/false)</returns>
+        public bool LigarAparelhoTraducao( string aparelhoLigado ){
 
-        public bool LigarAparelho( string aparelho ){
-
-            if(aparelho == "sim"){
+            //Caso o usuário deseja ligar ou não
+            if(aparelhoLigado == "sim"){
 
                 estadoAparelho = true;
             }else{
@@ -28,9 +33,15 @@ namespace Usando_celular
             return estadoAparelho;
         }
 
-        public bool DesligarAparelho( string aparelho ){
+        /// <summary>
+        /// Método para interpretar se o usuário quer desligar o aparelho
+        /// </summary>
+        /// <param name="aparelho"></param>
+        /// <returns>Celular desligado(sim/true) ou continuar ligado(não/false)</returns>
+        public bool DesligarAparelho( string aparelhoDesligado ){
 
-            if(aparelho == "sim"){
+            //Caso o usuário deseja desligar ou não
+            if(aparelhoDesligado == "sim"){
 
                 estadoAparelho = false;
             }else{
@@ -41,6 +52,11 @@ namespace Usando_celular
             return estadoAparelho;
         } 
 
+        /// <summary>
+        /// Método para realizar uma ligação
+        /// </summary>
+        /// <param name="aparelho"></param>
+        /// <returns>Se é possivel fazer a ligação ou não</returns>
         public string FazendoChamada( bool aparelho ){
 
             if(estadoAparelho){
@@ -48,6 +64,7 @@ namespace Usando_celular
                     Console.WriteLine("\nQual o número que deseja ligar? (Não precisa informar o DDD)");
                     int numeroTelefone = Int32.Parse(Console.ReadLine());
 
+                    //Verificando se o número é compativél ou não
                     if(numeroTelefone.ToString().Length >= 8 && numeroTelefone.ToString().Length <= 9){
 
                         return "Realizando chamada";
@@ -62,6 +79,11 @@ namespace Usando_celular
             }
         }
 
+        /// <summary>
+        /// Método para realizar o envio de mansagem
+        /// </summary>
+        /// <param name="aparelho"></param>
+        /// <returns>Se é possivel enviar uma mensagem ou não</returns>
         public string EnviarMensagem( bool aparelho ){
 
             if(estadoAparelho){
@@ -69,6 +91,7 @@ namespace Usando_celular
                     Console.WriteLine("\nQual o número que deseja enviar uma mensagem? (Não precisa informar o DDD)");
                     int numeroTelefone = Int32.Parse(Console.ReadLine());
 
+                    //Verificando se o número é compativél ou não
                     if(numeroTelefone.ToString().Length >= 8 && numeroTelefone.ToString().Length <= 9){
 
                         return "Enviando mensagem";
--- 1ºTermo/Backend/Exercicios_p.o.o/Usando_celular/Program.cs
+++ 1ºTermo/Backend/Exercicios_p.o.o/Usando_celular/Program.cs
@@ -6,20 +6,26 @@ namespace Usando_celular
     {
         static void Main(string[] args)
         {
+            //Criando um novo objeto celular
             Celular motorola = new Celular();
 
+            //atribuindo as informações aos atributos do objeto
             motorola.corAparelho = "preto";
             motorola.modeloAparelho = "Moto G (segunda geração)";
             motorola.tamanhoAparelho = 4.3f;
 
+            // Chamando o método para ver se o usuário deseja ligar o aparelho
             Console.WriteLine("Deseja ligar seu aparelho ?");
-            motorola.estadoAparelho = motorola.LigarAparelho(Console.ReadLine());
+            motorola.estadoAparelho = motorola.LigarAparelhoTraducao(Console.ReadLine());
 
+            // Loop para caso o usuário não queira desligar o aparelho
             do{
 
+                //Verificando se o usuário deseja realizar alguma tarefa
                 Console.WriteLine("\nDeseja usar alguma função do seu aparelho?");
                 string opcaoUsar = Console.ReadLine();
 
+                //Caso queira, mostrar as opções de atividades do sistema
                 while(opcaoUsar == "sim"){
                     
                     int opcaoFuncao;
@@ -30,26 +36,30 @@ namespace Usando_celular
 
                     switch(opcaoFuncao){
 
+                        // Método para caso o usuário queira realizar uma ligação
                         case 1:
 
                             Console.WriteLine(motorola.FazendoChamada(motorola.estadoAparelho));
                             break;
 
+                        // Método para caso o usuário queira encaminhar uma mensagem
                         case 2:
 
                             Console.WriteLine(motorola.EnviarMensagem(motorola.estadoAparelho));
                             break;
 
+                        // Verificando se o usuário não queira realizar uma tarefa não listada
                         default:
                             Console.WriteLine("Função não reconhecida pelo sistema!");
                             break;
                     }
 
+                    //Verificando se o usuário ainda queira realizar tarefas
                     Console.WriteLine("\nDeseja fazer outra função?");
                     opcaoUsar = Console.ReadLine();
                 }
 
-
+                //Verificando se o usuário queira desligar o aparelho ou não
                 Console.WriteLine("\nDeseja desligar seu aparelho ?");
                 motorola.estadoAparelho = motorola.DesligarAparelho(Console.ReadLine());
 

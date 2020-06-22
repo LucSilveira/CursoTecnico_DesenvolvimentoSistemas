d037a92e944bff11de7b1e36f55b33f0e108ef26
LucSilveira <lucas1-portal@hotmail.com>
2020-06-22 09:31:54 -0300

    Subindo exemplo de construtores

---
+++ 1ºTermo/Backend/Exercicios_p.o.o/Construtores/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Construtores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto sapato = new Produto();
+
+            Produto chinelo = new Produto(789);
+            System.Console.WriteLine(chinelo.Codigo);
+
+            Produto sandalia = new Produto(568, "Havaianas", "Sandalinhas de praia", 58);
+            System.Console.WriteLine(sandalia.Codigo);
+            System.Console.WriteLine(sandalia.Nome);
+            System.Console.WriteLine(sandalia.Descricao);
+            System.Console.WriteLine(sandalia.Estoque);
+        }
+    }
+}

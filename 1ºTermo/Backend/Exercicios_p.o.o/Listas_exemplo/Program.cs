5b1047cee9ee499a0a1a5069a290284e795ee45f
LucSilveira <lucas1-portal@hotmail.com>
2020-06-29 11:25:17 -0300

    Subindo exemplo de listas

---
+++ 1ºTermo/Backend/Exercicios_p.o.o/Listas_exemplo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Listas_exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> produtos = new List<Produto>();
+
+            //Adicionando de forma mais manual possivel
+            Produto prod = new Produto();
+            prod.Codigo = 1;
+            prod.Nome = "Iphone X";
+            prod.Preco = 3500f;
+
+            // Adicionando os produtos na lista
+            produtos.Add(prod);
+
+            // Adicionando os produtos de forma mais automatica
+            produtos.Add(new Produto(2, "Xiaomi", 2200.90f));
+            produtos.Add(new Produto(3, "Galaxy", 1800.90f));
+
+
+            // Listando todos os produtos que contém na lista
+            foreach(Produto prd in produtos)
+            {
+                Console.WriteLine($"R$: {prd.Preco} - {prd.Nome}");
+            }
+
+
+            // como remover itens da lista
+            produtos.Remove(prod);
+
+            produtos.RemoveAt(1);
+        }
+    }
+}

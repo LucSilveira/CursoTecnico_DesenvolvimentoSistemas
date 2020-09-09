26fd81cecb94c4551b9e92f2b8c79f0a29879949
LucSilveira <lucas1-portal@hotmail.com>
2020-09-09 01:12:11 -0300

    Subindo exemplos de .Net Core com o Entity

---
+++ 2ºTermo/Backend/APIBoletim/Interfaces/IAluno.cs
@@ -0,0 +1,17 @@
+﻿using APIBoletim.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Interfaces
+{
+    interface IAluno
+    {
+        List<Aluno> ListarAluno();
+        Aluno CadastrarAluno(Aluno _aluno);
+        Aluno BuscarAluno(int _idAluno);
+        Aluno AlterarAluno(Aluno _aluno);
+        Aluno ExcluirAluno(Aluno _aluno);
+    }
+}
---
+++ 2ºTermo/Backend/APIBoletim/Repositories/AlunoRepositorie.cs
@@ -0,0 +1,37 @@
+﻿using APIBoletim.Domains;
+using APIBoletim.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Repositories
+{
+    public class AlunoRepositorie : IAluno
+    {
+        public List<Aluno> ListarAluno()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno BuscarAluno(int _idAluno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno CadastrarAluno(Aluno _aluno)
+        {
+            throw new NotImplementedException();
+        }
+        public Aluno AlterarAluno(Aluno _aluno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno ExcluirAluno(Aluno _aluno)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}

beb620154716d3babefe39df1ed134111a6bf210
Lucas Silveira <lucas1-portal@hotmail.com>
2020-09-20 18:01:43 -0300

    Subindo aplicação do Entity Framework

---
+++ 2ºTermo/Backend/EfCore/Domains/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EfCore.Domains
+{
+    public class Produto
+    {
+        // Para questão de segurança da aplicação é usado o Guid
+        // para evitar que seja identificado o id do objeto e evitar infrações de segurança
+        [Key]
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        public Produto()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
